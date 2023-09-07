7f529b76c5a867fa16ef82fb37ff85e04609e9b5
Fazekas Levente <levente.fazekas@gmail.com>
2023-09-07 18:39:27 +0200

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,20 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.firebase.crashlytics)
+    alias(libs.plugins.sqlDelight)
 }
 
 android {
     namespace = "hu.levente.fazekas.receiptscanner"
-    compileSdk = 34
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "hu.levente.fazekas.receiptscanner"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.compileSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
@@ -33,18 +37,23 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.0"
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+sqldelight{
+    databases{
+        create("ReceiptDatabase"){
+            packageName.set("hu.levente.fazekas")
         }
     }
 }
@@ -64,4 +73,15 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //SqlDelight
+    implementation(libs.android.driver)
+
+    //Ktor
+    implementation(libs.ktor.client.okhttp)
+
+    implementation(libs.kotlinx.coroutines.core)
+
+    //KotlinxDateTime
+    implementation(libs.kotlinx.datetime)
 }
