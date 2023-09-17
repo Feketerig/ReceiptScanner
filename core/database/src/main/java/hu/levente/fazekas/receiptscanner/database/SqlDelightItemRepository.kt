b65c4fda7ef5cb0a50c2b0c44b1bcec68226a509
Fazekas Levente <levente.fazekas@gmail.com>
2023-09-17 17:58:56 +0200

    Fix Foreign key constrain fail

--- core/database/src/main/java/hu/levente/fazekas/receiptscanner/database/SqlDelightItemRepository.kt
+++ core/database/src/main/java/hu/levente/fazekas/receiptscanner/database/SqlDelightItemRepository.kt
@@ -40,39 +40,51 @@ class SqlDelightItemRepository(
     }
 
     fun updateItem(newItemEntity: ItemEntity){
-        val oldItem = db.itemQueries.selectById(newItemEntity.id).executeAsOne()
-        db.itemQueries.deleteById(newItemEntity.id) //TODO Check this
-        if (oldItem.itemId != newItemEntity.itemId){
-            db.itemIdQueries.deleteById(oldItem.itemId)
-            db.itemIdQueries.insert(newItemEntity.name)
-        }else if (oldItem.name != newItemEntity.name){
-            db.itemIdQueries.update(newItemEntity.itemId, newItemEntity.name)
+        db.transaction {
+            val oldItem = db.itemQueries.selectById(newItemEntity.id).executeAsOne()
+            if (oldItem.itemId != newItemEntity.itemId) {
+                db.itemIdQueries.insert(newItemEntity.name)
+            } else if (oldItem.name != newItemEntity.name) {
+                db.itemIdQueries.update(newItemEntity.itemId, newItemEntity.name)
+            }
+            if (oldItem.categoryId != newItemEntity.category.id) {
+                db.itemCategoryQueries.deleteById(oldItem.categoryId)
+                db.itemCategoryQueries.insert(
+                    newItemEntity.category.name,
+                    newItemEntity.category.color
+                )
+            } else if (oldItem.categoryName != newItemEntity.category.name || oldItem.categoryColor != newItemEntity.category.color) {
+                db.itemCategoryQueries.update(
+                    newItemEntity.category.id,
+                    newItemEntity.category.name,
+                    newItemEntity.category.color
+                )
+            }
+            db.itemQueries.update(
+                itemName = newItemEntity.name,
+                categoryName = newItemEntity.category.name,
+                id = newItemEntity.id,
+                count = newItemEntity.count,
+                price = newItemEntity.price,
+                unit = newItemEntity.unit,
+                date = newItemEntity.date,
+                currency = newItemEntity.currency,
+                receiptId = newItemEntity.receiptId
+            )
+            if (oldItem.itemId != newItemEntity.itemId) {
+                db.itemIdQueries.deleteById(oldItem.itemId)
+            }
         }
-        if (oldItem.categoryId != newItemEntity.category.id){
-            db.itemCategoryQueries.deleteById(oldItem.categoryId)
-            db.itemCategoryQueries.insert(newItemEntity.category.name, newItemEntity.category.color)
-        }else if (oldItem.categoryName != newItemEntity.category.name ||  oldItem.categoryColor != newItemEntity.category.color){
-            db.itemCategoryQueries.update(newItemEntity.category.id, newItemEntity.category.name, newItemEntity.category.color)
-        }
-        db.itemQueries.update(
-            itemName = newItemEntity.name,
-            categoryName = newItemEntity.category.name,
-            id = newItemEntity.id,
-            count = newItemEntity.count,
-            price = newItemEntity.price,
-            unit = newItemEntity.unit,
-            date = newItemEntity.date,
-            currency = newItemEntity.currency,
-            receiptId = newItemEntity.receiptId
-        )
     }
 
     fun deleteItem(id: Long){
-        val itemId = db.itemQueries.selectById(id).executeAsOne().itemId
-        db.itemQueries.deleteById(id)
-        val itemsWithSameItemId = db.itemQueries.selectAllByItemId(itemId).executeAsList()
-        if (itemsWithSameItemId.isEmpty()){
-            db.itemIdQueries.deleteById(itemId)
+        db.transaction{
+            val itemId = db.itemQueries.selectById(id).executeAsOne().itemId
+            db.itemQueries.deleteById(id)
+            val itemsWithSameItemId = db.itemQueries.selectAllByItemId(itemId).executeAsList()
+            if (itemsWithSameItemId.isEmpty()){
+                db.itemIdQueries.deleteById(itemId)
+            }
         }
     }
 }
