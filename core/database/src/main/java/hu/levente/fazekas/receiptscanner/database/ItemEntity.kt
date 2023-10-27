58ea89c7466bbe06f671bbf195de443134024763
Fazekas Levente <levente.fazekas@gmail.com>
2023-10-27 11:06:42 +0200

    add default values to ItemEntity

--- core/database/src/main/java/hu/levente/fazekas/receiptscanner/database/ItemEntity.kt
+++ core/database/src/main/java/hu/levente/fazekas/receiptscanner/database/ItemEntity.kt
@@ -1,17 +1,18 @@
 package hu.levente.fazekas.receiptscanner.database
 
 import hu.levente.fazekas.currency.Currency
+import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 
 data class ItemEntity(
-    val id: Long,
-    val itemId : Long,
-    val name: String,
-    val quantity: Long,
-    val price: Double, //price per measurement unit
-    val unit: String, //measurement unit
-    val category: ItemCategoryEntity,
-    val date: Instant,
-    val currency: Currency,
-    val receiptId: Long
+    val id: Long = 0,
+    val itemId : Long = 0,
+    val name: String = "",
+    val quantity: Long = 0,
+    val price: Double = 0.0, //price per measurement unit
+    val unit: String = "", //measurement unit
+    val category: ItemCategoryEntity = ItemCategoryEntity(0, "", 0),
+    val date: Instant = Clock.System.now(),
+    val currency: Currency = Currency.HUF,
+    val receiptId: Long = 0
 )
